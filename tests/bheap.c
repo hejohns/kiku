5d32c95a9347e4abebda0e6c724e4b594648df97
Johnson He <hejohns@umich.edu>
2020-06-20 02:55:54 -0400

    TODO
    bheap.h still needs to be cleaned up, but the test looks good. Accessing the vtable is ok

--- tests/bheap.c
+++ tests/bheap.c
@@ -1,6 +1,19 @@
 #include <kiku/common.h>
 #include <kiku/bheap.h>
 
+static bool less_int(void *restrict left, void *restrict right){
+    return *(int *)left < *(int*)right;
+}
+
+static void push_into_pq(void *restrict pq, void *restrict value){
+    (*((struct priority_queue **)pq))->push(pq, value);
+}
+
 int main(){
+    bheap inst = bheap_init(less_int, 16, sizeof(int));
+    int tmp;
+    tmp = 1, bheap_push(&inst, &tmp);
+    tmp = 2, push_into_pq(&inst, &tmp);
+    bheap_free(&inst);
     return 0;
 }
