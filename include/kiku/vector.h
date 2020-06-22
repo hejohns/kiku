8eeab4fe17703b92ffc4b785543c497f74c141f5
Johnson He <hejohns@umich.edu>
2020-06-22 04:55:26 -0400

    boilerplate code is really not fun

--- include/kiku/vector.h
+++ include/kiku/vector.h
@@ -4,4 +4,41 @@
 
 #include <kiku/RandomAccessContainer.h>
 
+typedef struct vector{
+    void *const vable; //initialize to vector_vtable
+    void *arr;
+    size_t size;
+    const size_t elt_size;
+    size_t capacity;
+} vector;
+
+/* forward declare internal functions called by type required interface */
+static void *vector_begin(void *cont);
+static void *vector_end(void *cont);
+static void *vector_next(void *cont, void *node);
+static size_t vector_size(void *cont);
+static void vector_pushBack(void *cont, void *value);
+static void *vector_prev(void *cont, void *node);
+static void *vector_tail(void *cont);
+static void vector_pushFront(void *cont, void *value);
+static void *vector_at(void *cont, size_t index);
+
+struct vector_vtable{
+    struct RandomAccessContainer RandomAccessContainer;
+};
+static struct vector_vtable vector_vtable = {
+    .RandomAccessContainer = {
+        .begin = vector_begin,
+        .end = vector_end,
+        .next = vector_next,
+        .size = vector_size,
+        .pushBack = vector_pushBack,
+        .prev = vector_prev,
+        .tail = vector_tail,
+        .pushFront = vector_pushFront,
+        .at = vector_at
+    }
+};
+/* begin internal implementation details */
+
 #endif /* VECTOR_H */
