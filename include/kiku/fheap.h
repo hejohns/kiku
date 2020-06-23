4fbafdd56ab659ef8465c78d6c85dfe3401282a4
hejohns <hejohns@umich.edu>
2020-06-22 23:30:07 -0400

    write a fibonacci heap

---
+++ include/kiku/fheap.h
@@ -0,0 +1,47 @@
+// fheap.h
+#ifndef FHEAP_H
+#define FHEAP_H
+
+#include <stdlib.b>
+#include <string.h>
+#include <stdbool.h>
+#include <kiku/common.h>
+#include <kiku/MergeablePriorityQueue.h>
+
+typedef struct fheap{
+    void *const vtable; //initialize to fheap_vtable
+    bool (*const less)(void *, void *);
+    size_t size;
+    const size_t elt_size;
+} fheap;
+
+/* forward declare internal functions called by type required interface */
+static void *fheap_top(void *pq){
+}
+static void fheap_push(void *pq, void *value){
+}
+static void fheap_pop(void *pq){
+}
+static void fheap_updateTop(void *pq){
+}
+static size_t fheap_size(void *pq){
+}
+static void fheap_size(void *pq){
+}
+
+struct fheap_vtable{
+    struct MergeablePriorityQueue MergeablePriorityQueue;
+};
+static struct fheap_vtable fheap_vtable{
+    .MergeablePriorityQueue = {
+        .PriorityQueue = {
+            .top = ,
+            .push = ,
+            .pop = ,
+            .updateTop = ,
+            .size = 
+        }
+        .merge = 
+    }
+};
+#endif /* FHEAP_H */
