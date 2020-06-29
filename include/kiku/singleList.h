df332872e52d418b19a422c956cf5410158045c6
hejohns <hejohns@umich.edu>
2020-06-29 00:37:00 -0400

    first write of singleList done
    note singleList is unsuitable for a circular list. I'm not sure exactly how many functions doubleList can use (can merge be used?)
    but singleList will leak memory if used as a circular list. The only ways to fix this involved using tail pointers, but in a singly
    linked list, this provides more nuisance than aid.

--- include/kiku/singleList.h
+++ include/kiku/singleList.h
@@ -89,11 +89,14 @@ static void *singleList_next(void *cont, void *node){
 
 static inline void singleList_clear_internal(singleList *cont){
     for(void *tmp = cont->head, *after_tmp; tmp && tmp != cont->head; tmp = after_tmp){
+        /* tmp != cont->head check is irrelevant to singleList since singleList is
+         * unsuitable for a circular list (ex pushFront will leak nodes).
+         * However, doubleList greatly appreciates this check, so this check must remain.
+         */
         *after_tmp = singleList_next(cont, tmp);
         free(tmp);
     }
     cont->head = NULL;
-    cont->tail = NULL;
     cont->size = 0;
 }
 static void singleList_clear(void *cont){
@@ -121,70 +124,29 @@ static inline void singleList_insertAfter_internal(singleList *cont, void *node,
     if(node){
         void *new_node = kiku_malloc(singleList_node_size_internal(cont));
         memcpy(new_node, value, cont->elt_size);
-        *singleList_prev_internal(cont, new_node) = node;
         void *node_after = singleList_next(cont, node);
-        *singleList_next_internal(cont, new_node) = node_after;
         *singleList_next_internal(cont, node) = new_node;
-        if(node_after){
-            *singleList_prev_internal(cont, node_after) = new_node;
-        }
-        else{
-            cont->tail = new_node;
-        }
-        cont->size++;
+        *singleList_next_internal(cont, new_node) = node_after;
     }
     else{ //to allow insertAfter on begin on an empty list
         assert(cont->size == 0);
         void *new_node = kiku_malloc(singleList_node_size_internal(cont));
         memcpy(new_node, value, cont->elt_size);
-        *singleList_prev_internal(cont, new_node) = NULL;
         *singleList_next_internal(cont, new_node) = NULL;
         cont->begin = new_node;
-        cont->tail = new_node;
-        cont->size++;
     }
+    cont->size++;
 }
 static void singleList_insertAfter(void *cont, void *node, void *value){
     singleList_insertAfter_internal(cont, node, value);
 }
 
-static inline void singleList_insertBefore_internal(singleList *cont, void *node, void *value){
-    if(node){
-        void *new_node = kiku_malloc(singleList_node_size_internal(cont));
-        memcpy(new_node, value, cont->elt_size);
-        *singleList_next_internal(cont, new_node) = node;
-        void *node_before = singleList_prev(cont, node);
-        *singleList_prev_internal(cont, new_node) = node_before;
-        *singleList_prev_internal(cont, node) = new_node;
-        if(node_before){
-            *singleList_next_internal(cont, node_before) = new_node;
-        }
-        else{
-            cont->head = new_node;
-        }
-        cont->size++;
-    }
-    else{ //to act like stl's insert before end iterator
-        singleList_insertAfter(cont, cont->tail, value);
-    }
-}
-static void singleList_insertBefore(void *cont, void *node, void *value){
-    singleList_insertBefore_internal(cont, node, value);
-}
-
 static inline void singleList_eraseAfter_internal(singleList *cont, void *node){
-    assert(cont->size > 0);
-    assert(node);
+    assert(cont->size > 0 && node);
     void *node_after = singleList_next(cont, node);
     assert(node_after);
     void *node_after_after = singleList_next(cont, node_after);
     *singleList_next_internal(cont, node) = node_after_after;
-    if(node_after_after){
-        *singleList_prev_internal(cont, node_after_after) = node;
-    }
-    else{
-        cont->tail = node;
-    }
     free(node_after);
     cont->size--;
 }
@@ -192,63 +154,37 @@ static void singleList_eraseAfter(void *cont, void *node){
     singleList_eraseAfter_internal(cont, node);
 }
 
-static inline void singleList_eraseBefore_internal(singleList *cont, void *node){
+static void singleList_pushFront_internal(singleList *cont, void *value){
+    void *new_node = kiku_malloc(singleList_node_size_internal(cont));
+    memcpy(new_node, value, cont->elt_size);
+    *singleList_next_internal(cont, new_node) = cont->head;
+    cont->head = new_node;
+    cont->size++;
+}
+static void singleList_pushFront(void *cont, void *value){
+    singleList_pushFront_internal(cont, value);
+}
+
+static void singleList_popFront_internal(singleList *cont){
     assert(cont->size > 0);
-    if(node){
-        void *node_before = singleList_prev(cont, node);
-        assert(node_before);
-        void *node_before_before = singleList_prev(cont, node_before);
-        *singleList_prev_internal(cont, node) = node_before_before;
-        if(!node_before_before){
-            cont->head = node;
-        }
-        free(node_before);
-    }
-    else{ //act as erase end, i.e. erase tail
-        void *node_before = singleList_prev(cont->tail);
-        if(node_before){
-            free(cont->tail);
-            *singleList_next_internal(cont, node_before) = NULL;
-            cont->tail = node_before;
-        }
-        else{
-            assert(cont->size == 1);
-            singleList_clear(cont);
-            return;
-        }
-    }
+    void *node_after = singleList_next(cont, cont->begin);
+    free(cont->begin);
+    cont->begin = node_after;
     cont->size--;
 }
-static void singleList_eraseBefore(void *cont, void *node){
-    singleList_eraseBefore_internal(cont, node);
-}
-
-static void singleList_pushFront(void *cont, void *value){
-    singleList_insertBefore(cont, ((singleList *)cont)->head, value);
-}
-
 static void singleList_popFront(void *cont){
-    singleList_eraseBefore(cont, singleList_next(cont, ((singleList *)cont)->head));
-}
-
-static void *singleList_tail(void *cont){
-    return ((singleList *)cont)->tail;
-}
-
-static void singleList_pushBack(void *cont, void *value){
-    singleList_insertBefore(cont, singleList_end(cont), value);
-}
-
-static void singleList_popBack(void *cont){
-    singleList_eraseBefore(cont, singleList_end(cont));
+    singleList_popFront_internal(cont);
 }
 
 static inline void singleList_merge_internal(singleList *cont, singleList *cont_other){
-    *singleList_next_internal(cont, cont->tail) = cont_other->head;
-    cont->tail = cont_other->tail;
+    void *cont_tail;
+    for(cont_tail = cont->begin;
+            singleList_next(cont, cont_tail);
+            cont_tail = singleList_next(cont, cont_tail)){
+    }
+    *singleList_next_internal(cont, cont_tail) = cont_other->head;
     cont->size += cont_other->size;
     cont_other->head = NULL;
-    cont_other->tail = NULL;
     cont_other->size = 0;
 }
 static void singleList_merge(void *cont, void *cont_other){
