70f1c15015a92c3d77d05b03aea1fbd2544bcd4a
hejohns <hejohns@umich.edu>
2020-06-29 20:10:03 -0400

    replaced bheap.c test with PriorityQueue.c
    The test doesn't work. But someday it will.

---
+++ benchtests/PriorityQueue.c
@@ -0,0 +1,33 @@
+// PriorityQueue.c
+
+#include <assert.h>
+
+#include <kiku/PriorityQueue.h>
+
+#ifndef TYPE
+#error
+#endif
+
+#define CALL_HELPER2(a, b) a ## _ ## b
+#define CALL_HELPER(a, b) CALL_HELPER2(a, b)
+#define CALL(x) CALL_HELPER(TYPE, x)
+
+static bool less_int(void *left, void *right){
+    return *(int *)left < *(int*)right;
+}
+
+int main(){
+    TYPE inst = CALL(init)(less_int, 16, sizeof(int));
+    int tmp;
+    srand(0);
+    size_t sz = 1<<24;
+    for(size_t i=0; i<sz; i++){
+        tmp = rand()%100;
+        PriorityQueue_push(&inst, &tmp);
+    }
+    for(size_t i=0; i<sz; i++){
+        PriorityQueue_pop(&inst);
+    }
+    CALL(free)(&inst);
+    return 0;
+}
