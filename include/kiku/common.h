986953cc0d7499543618fb03ecd581c912b3fc0d
hejohns <hejohns@umich.edu>
2020-07-05 07:11:05 -0400

    added duff's device option to memswp and turned for loops into do-while, throwing assert on size==0

--- include/kiku/common.h
+++ include/kiku/common.h
@@ -4,25 +4,63 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 /* define KIKU_USE_VLA to use vla/memcpy for memswp
+ *
+ * define KIKU_USE_DUFFS to use duff's device for memswp
+ * May be slightly faster when types to be swapped are larger
  *
  * default is as in glibc, to be safe and only use a single char buffer
  */
 static inline void memswp(void *restrict a, void *restrict b, size_t size){
-#ifdef KIKU_USE_VLA
+    assert(("memswp breaks on size == 0 to improve performance", size));
+#if !defined(__STDC_NO_VLA__) && defined(KIKU_USE_VLA)
     char tmp[size];
     memcpy(tmp, a, size);
     memcpy(a, b, size);
     memcpy(b, tmp, size);
+#elif defined(KIKU_USE_DUFFS)
+#define MEMSWP_SWAP                                    \
+    tmp = *(char *)a;                                  \
+    *(char *)a = *(char *)b;                           \
+    a = (char *)a + 1;                                 \
+    *(char *)b = tmp;                                  \
+    b = (char *)b + 1;                                 \
+    (void)0
+
+    size_t iter = (size + 15)/16;
+    register char tmp;
+    switch(size % 16){ //use power of 2 for bitmask modulo
+        do{
+        case 0: MEMSWP_SWAP;
+        case 15: MEMSWP_SWAP;
+        case 14: MEMSWP_SWAP;
+        case 13: MEMSWP_SWAP;
+        case 12: MEMSWP_SWAP;
+        case 11: MEMSWP_SWAP;
+        case 10: MEMSWP_SWAP;
+        case 9: MEMSWP_SWAP;
+        case 8: MEMSWP_SWAP;
+        case 7: MEMSWP_SWAP;
+        case 6: MEMSWP_SWAP;
+        case 5: MEMSWP_SWAP;
+        case 4: MEMSWP_SWAP;
+        case 3: MEMSWP_SWAP;
+        case 2: MEMSWP_SWAP;
+        case 1: MEMSWP_SWAP;
+        } while(--iter);
+    }
+#undef MEMSWP_SWAP
 #else
-    for(; size; size--){
-        char tmp = *(char *)a;
+    register char tmp;
+    do{
+        tmp = *(char *)a;
         *(char *)a = *(char *)b;
         a = (char *)a + 1;
         *(char *)b = tmp;
         b = (char *)b + 1;
-    }
+    } while(--size);
 #endif
 }
 
