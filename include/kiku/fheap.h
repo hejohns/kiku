735e9a0604588b7912962a222bccc70e941f7cfa
hejohns <hejohns@umich.edu>
2020-07-08 01:47:45 -0400

    fheap: WIP
    Lots to note:
    -- finally got to start working on fheap. Still feeling unsure how bug-free vector.h is but
    I suppose I always feel like something is wrong right after finishing it. vector passes the
    (extremely meager) DirectionalContainer and BidirectionalContainer tests. I'll do a "test" by
    writing another type that depends on vector (maybe cstring?)
    -- Basing fheap of CLRS. The slightly tricky bit is that clist is not a "normal" circular linked
    list, so I'm still working on the imaginary fheap node definition, since just straight forward
    parent and child pointers don't fit the kiku system.
    -- I'm holding off from writing too much code before I fully internalize the fibonacci heap
    otherwise I'll do what I always do and have to rewrite massive portions. I haven't gotten to
    why a bool marked is necessary, but I don't intend to support all the operations CLRS targets,
    so I have the feeling we may be able to simplify a couple things.
    -- A side issue, there is a lot of padding slop when composing kiku types in eachother. Ex is
    say we want to build a fheap out of something like
    
    struct foo{
        uint256_t a;
        char b;
    };
    
    and we're talking x86 or something so a uint256_t has a pretty large alignment req.
    Then sizeof(struct foo) is going to count all of the padding after the char.
    Then this sizeof(struct foo) is used to calculate the sizeof(fheap_imaginary_node)
    which will then put even more padding slop when fheap_imaginary_node is used in
    each clist_imaginary_node.
    As you can see, the more layers of composition, the more and more padding ad infinitum
    This is I suppose not a problem per se since we knew this in advance, and made the sacrifice
    to avoid compiler madness, but it still isn't pretty.

--- include/kiku/fheap.h
+++ include/kiku/fheap.h
@@ -5,29 +5,31 @@
 #include <stdlib.b>
 #include <string.h>
 #include <stdbool.h>
+
 #include <kiku/common.h>
 #include <kiku/MergeablePriorityQueue.h>
+#include <kiku/CircularContainer.h>
+#include <kiku/clist.h>
 
 typedef struct fheap{
     void *const vtable; //initialize to fheap_vtable
     bool (*const less)(void *, void *);
     size_t size;
     const size_t elt_size;
+    clist root_list;
+    void *min;
 } fheap;
 
 /* forward declare internal functions called by type required interface */
-static void *fheap_top(void *pq){
-}
-static void fheap_push(void *pq, void *value){
-}
-static void fheap_pop(void *pq){
-}
-static void fheap_updateTop(void *pq){
-}
+static void *fheap_top(void *pq);
+static void fheap_push(void *pq, void *value);
+static void fheap_pop(void *pq);
+static void fheap_updateTop(void *pq);
 static size_t fheap_size(void *pq){
+    return ((fheap *)pq)->size;
 }
-static void fheap_size(void *pq){
-}
+static void fheap_size(void *pq);
+static void fheap_merge(void *pq, void *pq_other);
 
 struct fheap_vtable{
     struct MergeablePriorityQueue MergeablePriorityQueue;
@@ -35,19 +37,51 @@ struct fheap_vtable{
 static struct fheap_vtable fheap_vtable{
     .MergeablePriorityQueue = {
         .PriorityQueue = {
-            .top = ,
-            .push = ,
-            .pop = ,
-            .updateTop = ,
-            .size = 
+            .top = fheap_top,
+            .push = fheap_push,
+            .pop = fheap_pop,
+            .updateTop = fheap_updateTop,
+            .size = fheap_size
         }
-        .merge = 
+        .merge = fheap_merge
     }
 };
 /* begin internal implementation details */
 
+/* imaginary node definition (becomes datum of clist)
 struct fheap_node{
-    struct fheap_node *parent;
-    struct fheap_node *child;
+    char datum[elt_size];
+    clist *parent_list;
+    void *parent;
+    clist children; //size_t degree == CircularContainer_size(&children)
+    bool mark;
 };
+*/
+
+static inline size_t fheap_init_node_size_internal(size_t elt_size){
+    return (elt_size % sizeof(void *))?
+        elt_size - (elt_size % sizeof(void *)) + 2*sizeof(void *) + sizeof(clist) + sizeof(bool)
+        :
+        elt_size + sizeof(void *) + sizeof(clist) + sizeof(bool);
+}
+static inline fheap fheap_init(
+        size_t elt_size,
+        bool (*const less)(void *)(void *)
+        ){
+    return (fheap){
+        .vtable = fheap_vtable,
+        .less = less,
+        .size = 0,
+        .elt_size = fheap_init_node_size_internal(elt_size),
+        .min = NULL
+    };
+}
+
+static inline void fheap_free(fheap *pq){
+    for(void *tmp = pq->min; tmp; tmp = CircularContainer_next(tmp)){
+        if( == CircularContainer_tail()){
+        }
+    }
+}
+
 #endif /* FHEAP_H */
